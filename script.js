b72b08279037ff61a67b8e8ebf2b349fd1f1d210
Markus Veem <70939473+markusveem@users.noreply.github.com>
2022-04-19 12:33:08 +0300

    mõned fixid

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
- const firstGalleryPic = document.querySelector(".firstBlock");
+let changeLanguage = document.querySelector(".languageSelect");
+
+ window.onload = function(){
+    changeLanguage.addEventListener("click",changeLanguageFunc);
+
+}
+
+
+function changeLanguageFunc(){
+
+    if (changeLanguage.innerHTML == "EN") {
+        changeLanguage.innerHTML = "ET";
+    }else{
+        changeLanguage.innerHTML = "EN";
+    }
+
+}
+
+ 
+ 
+ /*const firstGalleryPic = document.querySelector(".firstBlock");
  firstGalleryPic.addEventListener("click",change);
 
  function change(){
      
- }
+ }*/
